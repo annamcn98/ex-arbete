2c7276d50ac0583a0255f0cb52cf7f1cda3dd814
Anna Nilsson <annamcn98@hotmail.com>
2022-12-27 10:02:12 +0100

    homepage test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Homepage />
     </div>
   );
 }
