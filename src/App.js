8f4aa59857e36a32b4852a2640375b2971eb3856
Anna Nilsson <annamcn98@hotmail.com>
2022-12-30 17:50:03 +0100

    header start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Header from './components/Header';
-import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Homepage />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+
 
 function Header() {
   return (
     <div>
 
-       <h1>Hej jag är en h1a</h1>
-    
+        <div>
+            <Logodiv>
+                
+            </Logodiv>
+        </div>
+        
         
     </div>
   )
 }
+
+    const Logodiv = styled.div `
+    background-color: aliceblue;
+    width: 100vw;
+    height: 2vh;
+    `
 
 export default Header
