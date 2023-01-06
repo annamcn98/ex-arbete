c7f4f0cdc7b8a70283700f9bb00a0335ecd95651
Anna Nilsson <annamcn98@hotmail.com>
2023-01-06 12:56:29 +0100

    Route dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Header from './components/Header';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Product from './pages/Product';
+import Products from './pages/Products';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header />
+        <Routes>
+          <Route path='/product' element={Product} ></Route>
+          <Route path='/products' element={Products} ></Route>
+        </Routes>
+      <Footer />
+      </BrowserRouter>    
     </div>
   );
 }
